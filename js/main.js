b4da6e98ac44b826502722eec9aee57dfd57a44f
Воробьев Глеб <dizelplay@gmail.com>
2023-06-16 19:27:48 +0300

    add getRandomDigit, isStringFit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function getRandomDigit(from, to) {
+    if (from >= to) {
+      throw new Error('The value from cannot exceed or equal the value to');
+    }
+    const min = Math.ceil(from);
+    const max = Math.floor(to);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function isStringFit(str, maxLength) {
+    return maxLength >= str.length;
+  }
+  getRandomDigit(1,0);
+  isStringFit('Hey!', 10);
